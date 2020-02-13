9d13ec331faed983129c9ec9316f9bd4f55d690a
Swatika Rawat <swatikame56@gmail.com>
2020-02-13 22:46:21 +0530

    Connected Search feature with Elasticsearch

--- demo-app/js/index.js
+++ demo-app/js/index.js
@@ -3,8 +3,19 @@ $(".nav .nav-link").on("click", function() {
   $(".content").hide();
   console.log($(this).attr('id'))
   $("."+$(this).attr('id')).show();
-  
+
   //Set active class
   $(".nav").find(".active").removeClass("active");
    $(this).addClass("active");
+});
+
+$("#search-form").on("submit", function() {
+
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:9200/agriculture/_search"
+  }).done(function(data) {
+    console.log("Done")
+    console.log(data);
+  })
 });
