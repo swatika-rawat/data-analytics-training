b2b838c2a17a0d56c6cec98fba470addaf1b3d2a
Swatika Rawat <swatikame56@gmail.com>
2020-02-15 14:35:10 +0530

    Displaying sample data

--- demo-app/js/index.js
+++ demo-app/js/index.js
@@ -1,3 +1,4 @@
+//Simple navigation routing
 $(".nav .nav-link").on("click", function() {
   //Show - hide content
   $(".content").hide();
@@ -8,6 +9,7 @@ $(".nav .nav-link").on("click", function() {
    $(this).addClass("active");
 });
 
+//Search functionality
 $("#search-form").on("submit", function() {
   $.ajax({
     method: "POST",
@@ -15,12 +17,50 @@ $("#search-form").on("submit", function() {
     contentType: "application/json",
     data: '{ "query": { "match_all": {} } }'
   }).done(function(data) {
-    $('#results-table > tbody').find('tr').remove();
     console.log(data);
-    $("#results-div").show();
-    for (hit of data.hits.hits) {
-      console.log(hit._index);
-    }
+    $("#results-error-div").hide();
+    $("#results-success-div").show();
+
+    //Remove previous rows if any
+    $('#results-table > tbody').find('tr').remove();
+    //Add new row with received data
     $('#results-table > tbody:last-child').append('<tr><td>' + data.hits.total.value+ '</td><td>' + data.took + '</td></tr>');
+
+    //Remove previous sample data rows
+    $('#sample-data-table > tbody').find('tr').remove();
+    //Create sample data rows
+    max_sample_data_rows = 5;
+    total_sample_rows = 0;
+    rows_string = "";
+    for (hit of data.hits.hits) {
+      if(total_sample_rows < max_sample_data_rows) {
+        row_string = "<tr>";
+        bucket_data = hit._source;
+
+        if(total_sample_rows == 0) { //Add header row
+          for (item in bucket_data) {
+            row_string = row_string + "<td>"+item+"</td>";
+          }
+          row_string = row_string + "</tr><tr>"
+        }
+
+        for (item in bucket_data) {
+          row_string = row_string + "<td>"+bucket_data[item]+"</td>";
+        }
+        total_sample_rows++;
+        row_string = row_string + "</tr>";
+        rows_string = rows_string + row_string;
+      }
+      else {
+        break;
+      }
+    }
+    console.log(rows_string);
+    //Append sample data rows
+    $('#sample-data-table > tbody:last-child').append(rows_string);
+  }).fail(function(data) {
+    $("#results-success-div").hide();
+    $("#results-error-div").show();
+    console.log("Error");
   })
 });
