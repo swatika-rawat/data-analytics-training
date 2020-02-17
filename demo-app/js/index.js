d3777af84db1df2819b6183c6a890a443f02ab67
Swatika Rawat <swatikame56@gmail.com>
2020-02-17 11:28:44 +0530

    Changes in visualization using Chart.js

--- demo-app/js/index.js
+++ demo-app/js/index.js
@@ -66,3 +66,49 @@ $("#search-form").on("submit", function() {
 });
 
 //visualize
+$("#visualize-form").on("submit", function() {
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:9200/" + $('#visualizeIndexInput').val() + "/_search",
+    contentType: "application/json",
+    data: '{ "size": 0, "query": { "match_all": {} }, "aggs": { "aggrigation": { "terms": { "field": "' + $('#visualizeTextInput').val() + '.keyword", "size": ' + $('#visualizeSizeInput').val() +'  } } } }'
+  }).done(function(data) {
+    console.log(data);
+    $("#chart-error-div").hide();
+    $("#chart-success-div").show();
+    //Creating chart
+    var keys = [];
+    var doc_counts = []
+    for (bucket of data.aggregations.aggrigation.buckets) {
+      keys.push(bucket.key);
+      doc_counts.push(bucket.doc_count);
+    }
+    console.log(keys);
+    console.log(doc_counts);
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: keys,
+            datasets: [{
+                label: 'Count',
+                data: doc_counts,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }).fail(function(data) {
+    $("#chart-success-div").hide();
+    $("#chart-error-div").show();
+    console.log("Error");
+  })
+});
